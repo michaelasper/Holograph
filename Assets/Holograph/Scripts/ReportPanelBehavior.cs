5d97538d193f16ca232e0f5f1380035218181c4b
Michael Asper <t-miaspe@microsoft.com>
2017-07-17 13:47:13 -0700

    added report panel

---
+++ Assets/Holograph/Scripts/ReportPanelBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ReportPanelBehavior : MonoBehaviour {
+
+    public RectTransform progressBar;
+    public Text percentTextLabel;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var progress = progressBar.sizeDelta.x;
+        var progressFraction = progress / .25f;
+        percentTextLabel.text = ((int)(progressFraction * 100)).ToString() + "%";
+	}
+}
