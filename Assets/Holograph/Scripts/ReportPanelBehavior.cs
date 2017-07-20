3f1bbb32673b2e4828edcd27ff9d0f5dfc5bd78b
Michael Asper <t-miaspe@microsoft.com>
2017-07-20 09:45:07 -0700

    merged

--- Assets/Holograph/Scripts/ReportPanelBehavior.cs
+++ Assets/Holograph/Scripts/ReportPanelBehavior.cs
@@ -31,8 +31,4 @@ public class ReportPanelBehavior : MonoBehaviour
         var progressFraction = progress / .25f;
         percentTextLabel.text = ((int)(progressFraction * 100)).ToString() + "%";
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 1bc1a197fdfee26f33d8d1eea1a7c81f37ae1a2b
