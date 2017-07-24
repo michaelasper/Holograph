f672f23b72232fa89b0c694aa22581b7abeb623d
Ming Zhang <t-mingzh@microsoft.com>
2017-07-24 16:55:25 -0700

    add menu design

---
+++ Assets/Holograph/Scripts/ClickSwitchLight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using System;
+
+namespace Holograph
+{
+    public class ClickSwitchLight : MonoBehaviour, IInputHandler
+    {
+        public GameObject lightSource;
+        void IInputHandler.OnInputDown(InputEventData eventData)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IInputHandler.OnInputUp(InputEventData eventData)
+        {
+            lightSource.SetActive(!lightSource.activeSelf);
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
