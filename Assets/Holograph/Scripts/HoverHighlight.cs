c43fcfde8ebf8ec48689a18ecac5937f5d4a5f99
Ming Zhang <t-mingzh@microsoft.com>
2017-07-24 17:55:39 -0700

    add hexmenu (no functionality)

---
+++ Assets/Holograph/Scripts/HoverHighlight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverHighlight : MonoBehaviour
+{
+    [Range(1f, 2f)]
+    public float hightlight = 1.5f;
+    private bool isGazedAt;
+    private Transform cam;
+    private Material objectMaterial;
+    // Use this for initialization
+    void Start()
+    {
+        cam = Camera.main.transform;
+        objectMaterial = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RaycastHit hit;
+        Ray ray = new Ray(cam.position, cam.forward);
+        if (Physics.Raycast(ray, out hit) && hit.transform == this.transform)
+        {
+            if (!isGazedAt)
+            {
+                isGazedAt = true;
+                objectMaterial.color *= hightlight;
+            }
+        }
+        else if (isGazedAt)
+        {
+            isGazedAt = false;
+            objectMaterial.color /= hightlight;
+        }
+    }
+}
