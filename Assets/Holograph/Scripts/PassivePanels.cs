bc04bc18a0d9488feda6d6b95fe4521594141924
sidb <sidb@microsoft.com>
2017-07-25 13:54:58 -0700

    Add some passive panels on each side with some dynamic content

---
+++ Assets/Holograph/Scripts/PassivePanels.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PassivePanels : MonoBehaviour {
+    public Text Attendees;
+    public Text Cases;
+    public int Interval = 5;
+    private string _attendeeScan = "Scanning active users...";
+    private string _casesScan = "Scanning active cases...";
+    private int _attendees = 19;
+    private int _cases = 1768;
+
+    private GameObject _graph;
+
+	// Use this for initialization
+	void Start () {
+        _graph = GameObject.FindGameObjectWithTag("graph");
+        _graph.SetActive(false);
+        Attendees.text = _attendeeScan;
+        Cases.text = _casesScan;
+        StartCoroutine(PanelLoop());		
+	}
+
+    private IEnumerator PanelLoop()
+    {
+        while(true)
+        {
+            ChangeText();
+            yield return new WaitForSeconds(Interval);
+        }
+    }
+
+    private void ChangeText()
+    {
+        if (Attendees.text  == _attendeeScan && Cases.text == _casesScan)
+        {
+            Attendees.text = ($"Attendees: {_attendees}");
+            Cases.text = ($"Active Cases: {_cases}");
+            _graph.SetActive(true);
+        }
+        else
+        {
+            _attendees++;
+            _cases = _cases + 2;
+            Attendees.text = _attendeeScan;
+            Cases.text = _casesScan;
+            _graph.SetActive(false);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
