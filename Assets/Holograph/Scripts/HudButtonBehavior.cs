70daed821bf63a85b304845c7af7eef20f1bdb7a
Ming Zhang <t-mingzh@microsoft.com>
2017-07-26 14:12:50 -0700

    add hud and hover/click effects

---
+++ Assets/Holograph/Scripts/HudButtonBehavior.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+namespace Holograph
+{
+    public class HudButtonBehavior : MonoBehaviour,
+                                            IFocusable,
+                                            IInputClickHandler
+    {
+        public Texture idleTexture;
+        private Material buttonMaterial;
+        public Texture hoverTexture;
+        public Texture selectedTexture;
+        private int texturePropertyId;
+        private bool isSelected;
+        private bool isGazedAt;
+        private HudManager hudManager;
+
+        public void OnFocusEnter()
+        {
+            isGazedAt = true;
+            if (!isSelected)
+            {
+                buttonMaterial.SetTexture(texturePropertyId, hoverTexture);
+            }
+        }
+
+        public void OnFocusExit()
+        {
+            isGazedAt = false;
+            if (!isSelected)
+            {
+                buttonMaterial.SetTexture(texturePropertyId, idleTexture);
+            }
+        }
+
+        public void OnInputClicked(InputClickedEventData eventData)
+        {
+            hudManager.selectButton(transform);
+        }
+
+        public void switchSelected(bool select)
+        {
+            isSelected = select;
+            if (select)
+            {
+                buttonMaterial.SetTexture(texturePropertyId, selectedTexture);
+            }
+            else
+            {
+                buttonMaterial.SetTexture(texturePropertyId, isGazedAt ? hoverTexture : idleTexture);
+            }
+        }
+
+        void Start()
+        {
+            buttonMaterial = GetComponent<MeshRenderer>().material;
+            texturePropertyId = Shader.PropertyToID("_MainTex");
+            hudManager = GetComponentInParent<HudManager>();
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Holograph/Scripts/HudManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Holograph
+{
+    public class HudManager : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+        
+        public void selectButton(Transform clickedButton)
+        {
+            for (int i = 0; i < transform.childCount; ++i)
+            {
+                Transform childButton = transform.GetChild(i);
+                childButton.GetComponent<HudButtonBehavior>().switchSelected(clickedButton == childButton);
+            }
+        }
+    }
+}
