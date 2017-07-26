1542766055fabaea367336868f35d14435314fd7
sidb <sidb@microsoft.com>
2017-07-26 13:10:03 -0700

    Add script with methods for showing flyout panels

--- Assets/Holograph/Scripts/HUDFlyOutController.cs
+++ Assets/Holograph/Scripts/HUDFlyOutController.cs
@@ -1,16 +1,43 @@
-﻿using System.Collections;
+﻿using HoloToolkit.Unity;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HUDFlyOutController : MonoBehaviour {
+public class HUDFlyOutController : Singleton<HUDFlyOutController> {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public GameObject Level1;
+    public GameObject Level2;
+    public GameObject Level3;
+
+    public HUDFlyOutController()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        Level1.SetActive(false);
+        Level2.SetActive(false);
+        Level3.SetActive(false);
+    }
+
+    public void ShowLevel1()
+    {
+        Level1.SetActive(true);
+    }
+
+    public void ShowLevel2()
+    {
+        Level2.SetActive(true);
+    }
+
+    public void ShowLevel3()
+    {
+        Level3.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
