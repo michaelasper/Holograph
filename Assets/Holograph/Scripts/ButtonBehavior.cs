11c5f57d133aba56101ee13c9a3ca65bb5669db7
Ming Zhang <t-mingzh@microsoft.com>
2017-07-28 13:56:40 -0700

    reset menu buttons after clicked

--- Assets/Holograph/Scripts/ButtonBehavior.cs
+++ Assets/Holograph/Scripts/ButtonBehavior.cs
@@ -8,20 +8,15 @@ namespace Holograph
     public class ButtonBehavior : MonoBehaviour, IInputHandler, IFocusable
     {
         public Color hoverHighlight;
-        private bool isGazedAt;
         private Transform cam;
         private Material objectMaterial;
-        //public bool ButtonPressed;
         private string buttonName;
         public string methodName;
         private string color;
         private string texture;
         private MenuBehavior menuBehavior;
-        //public Animator IconAnimator;
-        // Use this for initialization
         void Start()
         {
-            //ButtonPressed = false;
             cam = Camera.main.transform;
             objectMaterial = GetComponent<MeshRenderer>().material;
             menuBehavior = transform.parent.GetComponent<MenuBehavior>();
@@ -29,18 +24,6 @@ namespace Holograph
 
         void Update()
         {
-
-            //RaycastHit hit;
-            //Ray ray = new Ray(cam.position, cam.forward);
-            //if (Physics.Raycast(ray, out hit) && hit.transform == this.transform)
-            //{
-            //    if (!isGazedAt)
-            //    {
-            //    }
-            //}
-            //else if (isGazedAt)
-            //{
-            //}
 
         }
 
@@ -51,25 +34,19 @@ namespace Holograph
 
         public void OnInputUp(InputEventData eventData)
         {
-            //ButtonPressed = !ButtonPressed;
-            //IconAnimator.SetBool("Button_1", ButtonPressed);
-            //Debug.Log("button " + buttonName + " is clicked");
-            isGazedAt = false;
+            OnFocusExit();
             menuBehavior.CloseMenu();
             menuBehavior.Invoke(methodName, 0f);
             NetworkMessages.Instance.SendRadialMenuClickIcon(methodName);
-            //NetworkMessages.Instance.SendMenuAnimationHash(Animator.StringToHash("Button_1"), NetworkMessages.AnimationTypes.Boolean, ButtonPressed ? 1 : 0); 
         }
 
         public void OnFocusEnter()
         {
-            Debug.Log("Hover enter");
             objectMaterial.color = hoverHighlight;
         }
 
         public void OnFocusExit()
         {
-            Debug.Log("Hover exit");
             objectMaterial.color = Color.white;
         }
 
@@ -82,4 +59,3 @@ namespace Holograph
         }
     }
 }
-
